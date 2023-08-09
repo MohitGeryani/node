67b71c42d7a77417d18d38ba278f82df68fe1658
MohitGeryani <mohitgeryani@gmail.com>
2023-08-09 18:47:02 +0530

    OSModule

---
+++ osModule/index.js
@@ -0,0 +1,14 @@
+// Operating system module , we can get info of OS through this module
+
+
+
+
+const os = require("os");
+
+console.log(os.freemem()); // available memory 
+console.log(os.homedir()); // in what directory we are in 
+
+console.log(os.machine()); // what machihe we're in i.e x86_64  (64bit)
+
+
+
