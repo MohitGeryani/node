3df10c0fb3eb4afacd3b9f0b3fc2ee26b682c7b4
MohitGeryani <mohitgeryani@gmail.com>
2023-08-10 16:41:37 +0530

    api creation

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,17 @@
 // Now we'll make a server in node js 
 
+
 const http = require("http");
+
+const fs = require("fs");
+
+
+const JsonData = fs.readFileSync("api.json", "utf-8");
+
+// now we'll convert it into an object 
+
+const ObjectData = JSON.parse(JsonData);
+
 
 const server = http.createServer((request, response) => {
 if(request.url == "/") {
@@ -11,7 +22,7 @@ else if(request.url == "/download") {
     response.end("Hi this is new download page");
 }
 else if(request.url == "/about") {
-    response.end("Hi this is About page");
+    response.end(ObjectData[0].name);
 }
 else {
     
@@ -26,5 +37,16 @@ server.listen(3000, ()=> {
     console.log("Server Listening on Port: 3000")});
 
 
+
+
+
+
+
+
+
+
+
+
+    // Note: we can send the string data or Json data to the server but  , we cannot send the whole object to server (for that we need to convert object into JSON format)
 
 
---
+++ Server/jsontest.js
@@ -0,0 +1,24 @@
+// here we will create an api 
+const obj = {
+
+    name: "Mohit", 
+    age: 19,
+    address: "Jaipur"
+
+}
+
+// Lets convert this object into json format 
+
+const jsonData = JSON.stringify(obj);   // Json.stringify converts object into Json
+
+// console.log(obj.address);
+
+
+console.log(jsonData);           // api is always in the form of json  , when key and value both are in double quotes "" then its a json form which is valid for apis 
+
+
+// Converting Json into object
+
+const objectData = JSON.parse(jsonData);  // JSON. parse converts Json into object
+
+console.log(objectData);
